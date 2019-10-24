ab45d0a6c3729bc31b2690eeb5713603c6538f4f
MAKSIM <yaroshevichmaks@gmail.com>
2019-10-24 19:14:59 +0300

    update UnitTes

--- UnitTest/UnitTest/Triangle.cs
+++ UnitTest/UnitTest/Triangle.cs
@@ -8,17 +8,9 @@ namespace UnitTest
 {
     public class Triangle
     {
-        public bool Triangle_Inequality(double a, double b, double c)
+        public static bool Triangle_Inequality(double a, double b, double c)
         {
-            if (a > 0 && b > 0 && c > 0)
-            {
-                if (((a + b) > c) && ((a + c) > b) && ((b + c) > a))
-                    return true;
-                else
-                    return false;
-            }
-            else
-                return false;
+            return (a > 0 && b > 0 && c > 0) && ((a + b) > c) && ((a + c) > b) && ((b + c) > a);
         }
     }
 }
