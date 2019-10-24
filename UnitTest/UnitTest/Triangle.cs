4aa6ba54b2f8ec9c2d7f88e45c2f2f7a74dfcba9
MAKSIM <yaroshevichmaks@gmail.com>
2019-10-24 18:52:12 +0300

    UnitTest

---
+++ UnitTest/UnitTest/Triangle.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class Triangle
+    {
+        public bool Triangle_Inequality(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                if (((a + b) > c) && ((a + c) > b) && ((b + c) > a))
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+    }
+}
