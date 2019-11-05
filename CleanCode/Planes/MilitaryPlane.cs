bf5423b7aa4016870879fef00f581ed84982ef26
Maksim <43035620+yaroshvichmaksim@users.noreply.github.com>
2019-11-05 11:23:43 +0300

    CleanCode

---
+++ CleanCode/Planes/MilitaryPlane.cs
@@ -0,0 +1,38 @@
+﻿using Aircompany.Models;
+
+namespace Aircompany.Planes
+{
+    public class MilitaryPlane : Plane
+    {
+        public MilitaryType PlanType;
+
+        public MilitaryPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType type)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            PlanType = type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var plane = obj as MilitaryPlane;
+            return plane != null &&
+                   base.Equals(obj) &&
+                   PlanType == plane.PlanType;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1701194404;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + PlanType.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}",
+                    ", type=" + PlanType +
+                    '}');
+        }        
+    }
+}
